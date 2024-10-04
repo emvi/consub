155e75eb4b3b04142e7c3a2b34d728df22130576
Marvin Blum <marvin.blum@emvi.com>
2024-10-04 19:02:40 +0200

    Implementation.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # consub
+
 consub is a simple configuration substitution tool.
+
+## Usage
+
+Download the binary from the release section or compile it. Then run:
+
+```
+consub .vars.env output.tpl.txt output.txt
+```
+
+The `.vars.env` file must be in the following format:
+
+```
+VAR=test
+```
+
+The `output.tpl.txt` file can then be configured like this:
+
+```
+My ${VAR} configuration!
+```
+
+The resulting `output.txt` file will look like this:
+
+```
+My test configuration!
+```
+
+## License
+
+MIT
---
+++ cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		log.Println("Usage: consub <vars file> <template file> <output file>")
+		return
+	}
+
+	if os.Args[2] == os.Args[3] {
+		log.Println("Overwriting the template file is not allowed!")
+		return
+	}
+
+	vars, err := os.Open(os.Args[1])
+
+	if err != nil {
+		log.Println("Error opening variables file: ", err)
+		return
+	}
+
+	defer vars.Close()
+	scanner := bufio.NewScanner(vars)
+	scanner.Split(bufio.ScanLines)
+	substitute := make(map[string]string)
+
+	for scanner.Scan() {
+		key, value, found := strings.Cut(scanner.Text(), "=")
+
+		if found {
+			substitute[key] = value
+		}
+	}
+
+	tpl, err := os.ReadFile(os.Args[2])
+
+	if err != nil {
+		log.Println("Error reading template file: ", err)
+		return
+	}
+
+	tplStr := string(tpl)
+
+	for key, value := range substitute {
+		tplStr = strings.ReplaceAll(tplStr, fmt.Sprintf("${%s}", key), value)
+	}
+
+	if err := os.WriteFile(os.Args[3], []byte(tplStr), 0644); err != nil {
+		log.Println("Error writing file: ", err)
+		return
+	}
+}
